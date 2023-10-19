5f7e19a5a2a32d455abd3dac0039e6c91445b4dd
Andres Gonzalez <knameless27@gmail.com>
2023-10-19 15:34:34 -0400

    feat: first card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 import DefaultCard from "./components/defaultCard";
 import weatherApi from "./services/weatherApi.js";
-import "./App.css";
+import "./styles/App.css";
 
 function App() {
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
   const [weather, setWeather] = useState({
     clouds: { all: 100 },
-    coord: { lon: -73.9955, lat: 45.2209 },
     dt: 0,
     main: {
       feels_like: 287.28,
@@ -48,8 +47,10 @@ function App() {
   useEffect(() => {
     weatherApi.currentWeather({ lat, lon }).then((res) => {
       setWeather(res);
+      document.body.style.backgroundImage = getColor(res.weather[0].icon);
     });
   }, [lat, lon]);
+
   return (
     <div>
       <div className="cardContainer">
@@ -57,6 +58,23 @@ function App() {
       </div>
     </div>
   );
+}
+
+function getColor(icon) {
+  const weather = {
+    "01d": "linear-gradient(to right, #f6d365 0%, #fda085 100%)",
+    "02d": "linear-gradient(to right, #96fbc4 0%, #f9f586 100%)",
+    "03d": "linear-gradient(to right, #ebc0fd 0%, #d9ded8 100%)",
+    "04d": "linear-gradient(to right, #93a5cf 0%, #e4efe9 100%)",
+    "09d": "linear-gradient(to right, #4facfe 0%, #00f2fe 100%)",
+    "10d": "linear-gradient(to right, #6a11cb 0%, #2575fc 100%)",
+    "11d": "linear-gradient(to right, #30cfd0 0%, #330867 100%)",
+    "13d": "linear-gradient(to right, #cfd9df 0%, #e2ebf0 100%)",
+    "50d": "linear-gradient(to right, #accbee 0%, #e7f0fd 100%)",
+  };
+
+  if (weather.hasOwnProperty(icon)) return weather[icon];
+  return weather["01d"];
 }
 
 export default App;
--- src/components/defaultCard.jsx
+++ src/components/defaultCard.jsx
@@ -1,22 +1,68 @@
-import "./styles/defaultCard.css";
+import "../styles/defaultCard.css";
 
 function DefaultCard(props) {
   const weather = props.weather;
   const date = new Date(0);
   date.setUTCSeconds(weather.dt);
-
-  const trueDate = date.toISOString();
+  const weath = weather.weather[0] ?? {};
+  const trueDate = new Date(date.toISOString()).toLocaleString();
+  const image = getImage(weath.icon);
 
   return (
     <div className="card">
       <div className="info">
-        <h1>{weather.name}</h1>
-        clouds{weather.clouds.all}
-        time{trueDate}
+        <h1 className="city">
+          {weather.name} - {weather.sys.country}
+        </h1>
+        <div className="basicInfo">
+          <h4>clouds: {weather.clouds.all}</h4>
+          <h4>visibility: {weather.visibility}</h4>
+        </div>
+        <h5>last measurement: {trueDate}</h5>
+        <button className="Button">See another place</button>
       </div>
-      <div className="imagen"></div>
+      <div className="image">
+        <div className="containerTitleWeather">
+          <img className="weatherImage" src={image} alt="" />
+          <h2>{weath.main}</h2>
+        </div>
+        <h3>{weath.description}</h3>
+        <hr />
+        <div className="windData">
+          <h2 className="titleWind">wind</h2>
+          <h4 className="itemWind">speed: {weather.wind.speed}</h4>
+          <h4 className="itemWind">deg: {weather.wind.deg}</h4>
+          <h4 className="itemWind">gust: {weather.wind.gust}</h4>
+        </div>
+      </div>
     </div>
   );
+}
+
+function getImage(icon) {
+  const weather = {
+    "01d":
+      "https://cdn-icons-png.flaticon.com/512/2698/2698194.png?ga=GA1.1.1295202294.1695664206",
+    "02d":
+      "https://cdn-icons-png.flaticon.com/512/2932/2932604.png?ga=GA1.1.1295202294.1695664206",
+    "03d":
+      "https://cdn-icons-png.flaticon.com/512/3208/3208676.png?ga=GA1.1.1295202294.1695664206",
+    "04d":
+      "https://cdn-icons-png.flaticon.com/512/414/414927.png?ga=GA1.1.1295202294.1695664206",
+    "09d":
+      "https://cdn-icons-png.flaticon.com/512/1164/1164945.png?ga=GA1.1.1295202294.1695664206",
+    "10d":
+      "https://cdn-icons-png.flaticon.com/512/3093/3093390.png?ga=GA1.1.1295202294.1695664206",
+    "11d":
+      "https://cdn-icons-png.flaticon.com/512/1146/1146860.png?ga=GA1.1.1295202294.1695664206",
+    "13d":
+      "https://cdn-icons-png.flaticon.com/512/4040/4040796.png?ga=GA1.1.1295202294.1695664206",
+    "50d":
+      "https://cdn-icons-png.flaticon.com/512/3861/3861484.png?ga=GA1.1.1295202294.1695664206",
+  };
+
+  if (weather.hasOwnProperty(icon)) return weather[icon];
+  return weather["01d"];
 }
 
 export default DefaultCard;
